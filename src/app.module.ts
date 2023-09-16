e525badbd5f65a06e7670b4028f6233be26f614d
DoomsdayIS <ivan.sheremet.sus@bk.ru>
2023-09-16 13:39:01 +0300

    'fixes'

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-import { PostService } from './post.service';
 import { PrismaService } from './prisma.service';
 import { CommentsModule } from './comments/comments.module';
 import { UsersModule } from './users/users.module';
@@ -10,8 +8,8 @@ import { OrdersModule } from './orders/orders.module';
 import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [CatsModule, CommentsModule, UsersModule, OrdersModule, ItemsModule],
+  imports: [CommentsModule, UsersModule, OrdersModule, ItemsModule],
   controllers: [AppController],
-  providers: [AppService, PostService, PrismaService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
